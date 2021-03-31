bd030561b56b0660457ce4af02f1a45011d0c0d9
Hossam <hossam.ahmed.elkashef@gmail.com>
2021-03-31 20:49:01 +0200

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCXKq-YtHRG-8P76mksqjv4xmWtuBE6GTk",
+    authDomain: "hossam-el-kashef.firebaseapp.com",
+    projectId: "hossam-el-kashef",
+    storageBucket: "hossam-el-kashef.appspot.com",
+    messagingSenderId: "210729174104",
+    appId: "1:210729174104:web:772d057202f9bfb6e9362d",
+    measurementId: "G-1RY0Q99NGF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
